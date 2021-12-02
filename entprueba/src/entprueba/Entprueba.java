d4553561fbb913c8e17a452045600faa7f3a8512
davsan03 <davsan03@ubuntu>
2021-12-02 18:33:19 +0100

    El programa pedirá la temperatura de n municipios en lugar de 5, siendo el usuario el que indique el valor de n.

--- entprueba/src/entprueba/Entprueba.java
+++ entprueba/src/entprueba/Entprueba.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Entprueba {
     public static void main(String[] args) {
         System.out.println("Buenos dias");
-        System.out.println("Ingrese 5 temperaturas");
+        System.out.println("Ingrese el número de municipios");
         Scanner sctemp=new Scanner(System.in);
-        for (int cont=0; cont<5;++cont){
+        int municipios=sctemp.nextInt();
+        for (int cont=0; cont<municipios;++cont){
+        System.out.println("Ingrese temperatura");
         int temperatura=sctemp.nextInt();
         if (temperatura>=0){
               System.out.println("Temperatura positiva");
