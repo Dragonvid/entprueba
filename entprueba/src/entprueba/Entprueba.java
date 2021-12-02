59ac94c93d9e683d474d513b73f1dc72fb3117e0
davsan03 <davsan03@ubuntu>
2021-12-02 18:48:40 +0100

    El programa mostrará por pantalla la temperatura mayor y menor..

--- entprueba/src/entprueba/Entprueba.java
+++ entprueba/src/entprueba/Entprueba.java
@@ -2,6 +2,10 @@ package entprueba;
 import java.util.Scanner;
 public class Entprueba {
     public static void main(String[] args) {
+        int menor=0;
+        int mayor=0;
+        int menor2=0;
+        int mayor2=0;
         System.out.println("Buenos dias");
         System.out.println("Ingrese el número de municipios");
         Scanner sctemp=new Scanner(System.in);
@@ -14,7 +18,14 @@ public class Entprueba {
         }else{
               System.out.println("Temperatura negativa");
         }
+        if (temperatura<menor) {
+        menor=temperatura;
+        }else{
+        mayor=temperatura;
+        }
+        
      }
+      System.out.println("Temperatura mayor "+mayor+" Temperatura menor"+menor);
   }
     
 }
