4f177815e36d1ac62e77648ab9f0a6f020a45c6d
davsan03 <davsan03@ubuntu>
2021-12-09 19:04:03 +0100

    La temperatura menor no tendrá que ser 0 em caso de que no haya menor temperatura

--- entprueba/src/entprueba/Entprueba.java
+++ entprueba/src/entprueba/Entprueba.java
@@ -2,28 +2,28 @@ package entprueba;
 import java.util.Scanner;
 public class Entprueba {
     public static void main(String[] args) {
-        float menor=0;
-        float mayor=0;
+        float menor=5000;
+        float mayor=-5000;
         System.out.println("Buenos dias");
         System.out.println("Ingrese el número de municipios");
         Scanner sctemp=new Scanner(System.in);
         int municipios=sctemp.nextInt();
         for (int cont=0; cont<municipios;++cont){
-        System.out.println("Ingrese temperatura");
-        float temperatura=sctemp.nextFloat();
-        if (temperatura>=0){
-              System.out.println("Temperatura positiva");
-        }else{
-              System.out.println("Temperatura negativa");
-        }
-        if (temperatura<menor) {
-        menor=temperatura;
-        }else{
-        mayor=temperatura;
-        }
+            System.out.println("Ingrese temperatura");
+            float temperatura=sctemp.nextFloat();
+            if (temperatura>=0){
+                System.out.println("Temperatura positiva");
+            }else{
+                System.out.println("Temperatura negativa");
+            }
+            if (temperatura<menor) {
+                menor=temperatura;
+            }if(temperatura>mayor){
+                mayor=temperatura;
+            }
         
-     }
-      System.out.println("Temperatura mayor "+mayor+" Temperatura menor"+menor);
-  }
+        }
+        System.out.println("Temperatura mayor "+mayor+" Temperatura menor"+menor);
+    }
     
 }
