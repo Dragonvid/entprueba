dea791277776a00ce5091fdc54cf6e914fd10771
davsan03 <davsan03@ubuntu>
2021-12-16 18:18:22 +0100

    Hacer que guarde todas las temperaturas en un array para mostrarlas todas al final del programa.

--- entprueba/src/entprueba/Entprueba.java
+++ entprueba/src/entprueba/Entprueba.java
@@ -8,29 +8,35 @@ public class Entprueba {
         //Te he cambiado el double de mayor y menor por algo mucho mejor
         //Double.MIN_VALUE introduce en la variable el valor mas pequeño posible para los Double.
         //Double.MAX_VALUE introduce en la variable el valor mas grande posible para los Double.
+        double temperatura[];
         double mayor = Double.MIN_VALUE;
         double menor = Double.MAX_VALUE;
         System.out.println("Buenos dias");
         System.out.println("Ingrese el número de municipios");
         Scanner sctemp = new Scanner(System.in);
         int municipios = sctemp.nextInt();
+        temperatura = new double[municipios];
         for (int cont = 0; cont < municipios; ++cont) {
             System.out.println("Ingrese temperatura");
-            float temperatura = sctemp.nextFloat();
-            if (temperatura >= 0) {
+            temperatura[cont] = sctemp.nextDouble(); 
+            if (temperatura[cont] >= 0) {
                 System.out.println("Temperatura positiva");
             } else {
                 System.out.println("Temperatura negativa");
             }
-            if (temperatura < menor) {
-                menor = temperatura;
+            if (temperatura[cont] < menor) {
+                menor = temperatura[cont];
             }
-            if (temperatura > mayor) {
-                mayor = temperatura;
+            if (temperatura[cont] > mayor) {
+                mayor = temperatura[cont];
             }
 
         }
         System.out.println("La temperatura mayor es " + mayor + " y la temperatura menor es " + menor);
+        System.out.println("Las temperaturas introducidas son:");
+        for (int cont2 = 0; cont2 < municipios; ++cont2) {
+        System.out.println(temperatura[cont2]);
+        }
     }
 
 }
