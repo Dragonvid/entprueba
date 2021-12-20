d856ebdc1b0172da27654fc366ecf68cc95832f0
davsan03 <davsan03@ubuntu>
2021-12-20 17:31:53 +0100

    Ahora pide también el nombre de los municipios y los guarde en otro array para mostrarlos todos al final del programa.

--- entprueba/src/entprueba/Entprueba.java
+++ entprueba/src/entprueba/Entprueba.java
@@ -8,34 +8,41 @@ public class Entprueba {
         //Te he cambiado el double de mayor y menor por algo mucho mejor
         //Double.MIN_VALUE introduce en la variable el valor mas pequeño posible para los Double.
         //Double.MAX_VALUE introduce en la variable el valor mas grande posible para los Double.
-        double temperatura[];
+        double[] temperaturas;
+        String[] municipios ;
         double mayor = Double.MIN_VALUE;
         double menor = Double.MAX_VALUE;
+        Scanner sctemp = new Scanner(System.in);
         System.out.println("Buenos dias");
         System.out.println("Ingrese el número de municipios");
-        Scanner sctemp = new Scanner(System.in);
-        int municipios = sctemp.nextInt();
-        temperatura = new double[municipios];
-        for (int cont = 0; cont < municipios; ++cont) {
+        int numMunicipios = sctemp.nextInt();
+        temperaturas = new double[numMunicipios];
+        municipios = new String[numMunicipios];
+        for (int cont = 0; cont < numMunicipios; ++cont) {
+            System.out.println("Ingrese el nombre del municipio");
+            String a = sctemp.nextLine();
+            municipios[cont] = sctemp.nextLine(); 
             System.out.println("Ingrese temperatura");
-            temperatura[cont] = sctemp.nextDouble(); 
-            if (temperatura[cont] >= 0) {
+            temperaturas[cont] = sctemp.nextDouble(); 
+            if (temperaturas[cont] >= 0) {
                 System.out.println("Temperatura positiva");
             } else {
                 System.out.println("Temperatura negativa");
             }
-            if (temperatura[cont] < menor) {
-                menor = temperatura[cont];
+            if (temperaturas[cont] < menor) {
+                menor = temperaturas[cont];
             }
-            if (temperatura[cont] > mayor) {
-                mayor = temperatura[cont];
+            if (temperaturas[cont] > mayor) {
+                mayor = temperaturas[cont];
             }
 
         }
         System.out.println("La temperatura mayor es " + mayor + " y la temperatura menor es " + menor);
-        System.out.println("Las temperaturas introducidas son:");
-        for (int cont2 = 0; cont2 < municipios; ++cont2) {
-        System.out.println(temperatura[cont2]);
+        System.out.println("Las temperaturas y municipios introducidas son:");
+        for (int cont2 = 0; cont2 < numMunicipios; ++cont2) {
+        System.out.print(temperaturas[cont2]);
+        System.out.print(municipios[cont2]);
+        System.out.println("");
         }
     }
 
